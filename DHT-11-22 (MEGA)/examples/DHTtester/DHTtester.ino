75c50d54e59cfbbcad15f6f1c40e02ca2f423320
Thiago Cury <thiagocury@gmail.com>
2017-09-22 21:43:12 -0300

    Bibliotecas para os sensores DHT11-22 de temperatura e humidade

---
+++ DHT-11-22 (MEGA)/examples/DHTtester/DHTtester.ino
@@ -0,0 +1,34 @@
+#include "DHT.h"
+#define DHTPIN 2     
+#define DHTTYPE DHT11   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("DHT11 test!");
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Falha na leitura do sensor DHT!");
+    return;
+  }
+
+  float hi = dht.computeHeatIndex(f, h);
+  Serial.print("Umidade: "); 
+  Serial.print(h);
+  Serial.print(" %  ");
+  Serial.print("Temperatura: "); 
+  Serial.print(t);
+  Serial.print("*C  ");
+  Serial.print(f);
+  Serial.print("*F  ");
+  Serial.print("Indice de calor: ");
+  Serial.print(hi);
+  Serial.println("*F");
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DHT11-22
-O DHT11, 22 são sensores de temperatura e humidade.
+Aqui você pode baixar as libs para os sensores de temperatura e humidade DHT11, 22 para os arduinos UNO/MEGA.
+____________________
+
+This is an Arduino library for the DHT series of low cost temperature/humidity sensors. 
+
+To download. click the DOWNLOADS button in the top right corner, rename the uncompressed folder DHT. Check that the DHT folder contains DHT.cpp and DHT.h. Place the DHT library folder your <arduinosketchfolder>/libraries/ folder. You may need to create the libraries subfolder if its your first library. Restart the IDE.
